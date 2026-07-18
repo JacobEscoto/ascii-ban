d568b5d7db27c2b67326a7c3f8b6ce7d598db9b0
JacobEscoto <djacobescotoz@gmail.com>
2026-07-17 19:30:27 -0600

    update(font): implement validation to ensure that the font characters have the same height.
    - font.go: validates the height for each of the character of the font
    when adding.
    - standard.go: add new characters to the font

--- internal/font/font.go
+++ internal/font/font.go
@@ -3,7 +3,10 @@
 // Each font registered using AddFont must include the character '?' or a blank space as a fallback.
 package font
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // Font represents a struct that includes a name and a characters map that returns an array of strings.
 type Font struct {
@@ -18,6 +21,25 @@ var AvailableFonts = make(map[string]Font)
 func AddFont(font Font) error {
 	if _, exists := AvailableFonts[font.Name]; exists {
 		return errors.New("font already exists")
+	}
+
+	_, questionExists := font.Characters['?']
+	_, spaceExists := font.Characters[' ']
+
+	if !questionExists || !spaceExists {
+		return errors.New("please check your font before adding it. It is missing required characters such as '?' or ' '")
+	}
+
+	// Validate that all characters have the same height defined in `Font.Height`
+	invalidCharacters := []rune{}
+	for runeKey, lines := range font.Characters {
+		if len(lines) != font.Height {
+			invalidCharacters = append(invalidCharacters, runeKey)
+		}
+	}
+
+	if len(invalidCharacters) > 0 {
+		return fmt.Errorf("the following characters do not match the defined height of %d: %c", font.Height, invalidCharacters)
 	}
 
 	AvailableFonts[font.Name] = font
